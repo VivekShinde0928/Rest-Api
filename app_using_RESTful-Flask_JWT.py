e099ab0feb1545d17030e8f738e68c2d7079a350
VivekShinde0928 <65374853+VivekShinde0928@users.noreply.github.com>
2020-09-17 00:13:08 +0530

    edited

--- app_using_RESTful-Flask_JWT.py
+++ app_using_RESTful-Flask_JWT.py
@@ -13,12 +13,6 @@ app.secret_key = 'vick'
 api = Api(app)
 
 
-# by using this our table get created by name data.db & no need to make create_table.py
-@app.before_first_request
-def create_table():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)  # jwt creates new endpoint i.e. /auth
 
 api.add_resource(Item, '/item/<string:name>')
